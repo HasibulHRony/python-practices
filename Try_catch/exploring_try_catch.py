65532bc247367cf9d6aac628a52c7df3e8f21cbf
HasibulHRony <hasibulhrony7@gmail.com>
2026-04-20 10:13:21 +0600

    try except concepts.

---
+++ Try_catch/exploring_try_catch.py
@@ -0,0 +1,13 @@
+try:
+    number = int(input("Enter a number: "))
+    print("Your inputed number is: ", number)
+except ValueError as err:
+    print(err)
+
+
+
+try:
+    num = 11/0
+    print(num)
+except ZeroDivisionError as zeroErr:
+    print(zeroErr)
